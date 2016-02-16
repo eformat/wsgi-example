3e91709de508446d912d682a938be77fe0a69ed3
Maciej Szulik <maszulik@redhat.com>
2016-02-16 15:20:46 +0100

    save-artifacts

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World from WSGI app with custom scripts!"]
+    return [b"Hello World from WSGI app with save-artifacts!"]
 
